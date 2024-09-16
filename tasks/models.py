46fef4bafcda59c7c1056fa21fd621f38632318b
cristhian <kcaladj123@gmail.com>
2024-09-16 00:04:00 -0500

    modelo de tareas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+    def __str__(self):
+        tex = "{0} [{1}] ({2})"
+        return tex.format(self.title, self.description, self.done)
